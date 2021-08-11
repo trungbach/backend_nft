c2115ff84f0513e9bd64c20bab28ceef48ef23fc
Hoang Son <hoangson.0126@gmail.com>
2021-08-11 21:58:29 +0700

    changed condition's buy the item

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 
-  exports.nftmarketaddress = "0x97f8674bCD50b8e534d26882c004fa4d852254a5"
-  exports.nftaddress = "0xE68E76451E41a0A83E8bD93d0E3b308ec87A9318"
+  exports.nftmarketaddress = "0x806D9941D6BbFD5B72e8926840481009E3F7C7e8"
+  exports.nftaddress = "0x302ae930f7b27abd0D5889aa5260c24078254aF6"
   
