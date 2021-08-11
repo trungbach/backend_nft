20ce5be539405e3ec09af5d5b2d7a0169c69fecc
Hoang Son <hoangson.0126@gmail.com>
2021-08-11 15:42:31 +0700

    no message

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 
-  export const nftmarketaddress = "0x97f8674bCD50b8e534d26882c004fa4d852254a5"
-  export const nftaddress = "0xE68E76451E41a0A83E8bD93d0E3b308ec87A9318"
+  exports.nftmarketaddress = "0x97f8674bCD50b8e534d26882c004fa4d852254a5"
+  exports.nftaddress = "0xE68E76451E41a0A83E8bD93d0E3b308ec87A9318"
   
