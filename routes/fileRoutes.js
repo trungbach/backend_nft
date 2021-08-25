45e7736702a1f8ba77034477489cda3620b05ff7
Hoang Son <hoangson.0126@gmail.com>
2021-08-25 14:30:35 +0700

    fixed upload file

--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -60,23 +60,25 @@ module.exports = function (app) {
                 let original_file = req.files.original_file;
                 let original_name = `${timestamp}${original_file.name}`
                 //Use the mv() method to place the file in upload directory (i.e. "uploads")
-                original_file.mv('./public/uploads/' + original_file.name);
+                await original_file.mv('./public/uploads/' + original_file.name);
                 fs.rename(`./public/uploads/${original_file.name}`, `./public/uploads/${original_name}`, function(err) {
                     if ( err ) console.log('ERROR: ' + err);
                 });
 
-                let thumb_file = req.files.thumb_file;
-                let thumb_name = `${timestamp}${thumb_file.name}`
-                //Use the mv() method to place the file in upload directory (i.e. "uploads")
-                thumb_file.mv('./public/uploads/' + thumb_file.name);
-                fs.rename(`./public/uploads/${thumb_file.name}`, `./public/uploads/${thumb_name}`, function(err) {
-                    if ( err ) console.log('ERROR: ' + err);
-                });
+                if(req.body.type == File.VIDEO){
+                    let thumb_file = req.files.thumb_file;
+                    var thumb_name = `${timestamp}${thumb_file.name}`
+                    //Use the mv() method to place the file in upload directory (i.e. "uploads")
+                    thumb_file.mv('./public/uploads/' + thumb_file.name);
+                    fs.rename(`./public/uploads/${thumb_file.name}`, `./public/uploads/${thumb_name}`, function(err) {
+                        if ( err ) console.log('ERROR: ' + err);
+                    });
+                }
                 
                 //save file
                 var file_id = await File.createFile(new File({
                     original_url: `${req.protocol}://${req.get('host')}/public/uploads/${original_name}`,
-                    thumb_url: `${req.protocol}://${req.get('host')}/public/uploads/${thumb_name}`,
+                    thumb_url: req.body.type == File.VIDEO ? `${req.protocol}://${req.get('host')}/public/uploads/${thumb_name}`: '',
                     type: req.body.type
                 }))
                 var newFile = await File.getItemById(file_id);
